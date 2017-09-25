176693a6a2d3f8a9bcf25d4d587f6d744dc77463
Doug Black <dblack@twilio.com>
2017-09-25 12:44:50 -0700

    Remove fantasy render

--- scripts/render.py
+++ scripts/render.py
@@ -23,23 +23,3 @@ for file in markdown_files:
     html_filename = splitext(file)[0] + '.html'
     with open('words/%s' % html_filename, 'w+') as f:
         f.write(output)
-
-
-markdown_files = os.listdir('fantasy')
-
-for file in markdown_files:
-    with open("fantasy/%s" % file) as f:
-        content = f.read()
-
-    title = ' '.join(word.title() for word in file[:-3].split('-'))
-
-    md = markdown.markdown(content, extensions=['codehilite'])
-
-    template_loader = jinja2.FileSystemLoader(searchpath="templates")
-    template_env = jinja2.Environment(loader=template_loader)
-    template = template_env.get_template(TEMPLATE_FILE)
-    output = template.render(title=title, content=md)
-
-    html_filename = splitext(file)[0] + '.html'
-    with open('words/%s' % html_filename, 'w+') as f:
-        f.write(output)
